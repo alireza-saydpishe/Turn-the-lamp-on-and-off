edcaa17c4a6d912a2b2e664bdca6db093b05d367
saydpishe <alirezasaydpishe20@gmail.com>
2024-08-01 18:18:37 +0330

    c1

---
+++ app.js
@@ -0,0 +1,15 @@
+function trun_off() {
+    document.getElementById('demo').src = 'b10342dasdsadad.jpg'
+}
+function change() {
+    document.getElementById('demo').src = 'trun on.jpg'
+}
+
+function change_background_off() {
+    document.body.style.backgroundColor = 'rgba(64,64,64,1)';
+}
+
+function change_background_on(){
+    document.body.style.backgroundColor = '#fff'
+    document.body.style.backgroundBlendMode ='lighten' ;
+}
